bc8b34cc5de53609234c359a547756e8f2ea443e
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-07-18 12:13:44 -0500

    wip submitted for retro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+axios
+.get(`https://api.nasa.gov/planetary/apod`)
+.then(res =>{
+  console.log(res)
+})
+.catch((err) => {
+  console.log(err)
+});
+
   return (
     <div className="App">
       <p>
